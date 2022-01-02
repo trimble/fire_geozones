5c4ae20582d12400dac54f875f4a70a767bd7636
Will Trimble <wmt1276@rit.edu>
2022-01-02 00:48:14 -0500

    Reordered Stations to Tweak Voronoi Diagram Coloring

--- fire_geodata.py
+++ fire_geodata.py
@@ -6,7 +6,6 @@ stations = [
   {"name":"Shelbyville Station 691","lat":39.5227097,"lon":-85.7786288},
   {"name":"IFD Station 16","lat":39.6691316,"lon":-86.0065276},
   {"name":"Needham Station 82","lat":39.6040567,"lon":-85.9990409},
-  {"name":"Whiteland Station 71","lat":39.5484272,"lon":-86.0747433},
   {"name":"Fountaintown Station 612","lat":39.6937218,"lon":-85.779439},
   {"name":"IFD Station 55","lat":39.6962247,"lon":-85.9795233},
   {"name":"Morristown Station 651","lat":39.6760901,"lon":-85.7045637},
@@ -14,12 +13,13 @@ stations = [
   {"name":"Flat Rock Station 631","lat":39.3641328,"lon":-85.8309059},
   {"name":"Sugar Creek Township Station 45","lat":39.7338448,"lon":-85.9131422},
   {"name":"Manilla Station 61","lat":39.5710743,"lon":-85.617652},
+  {"name":"Whiteland Station 71","lat":39.5484272,"lon":-86.0747433},
   {"name":"Shelbyville Station 692","lat":39.51627,"lon":-85.7434899},
   {"name":"Shelbyville Station 693","lat":39.5055857,"lon":-85.7945173},
   {"name":"Franklin Station 23","lat":39.523629,"lon":-86.0737878},
+  {"name":"Greenwood Station 91","lat":39.6134433,"lon":-86.1040209},
   {"name":"St Paul Station 41","lat":39.4282778,"lon":-85.6285885},
   {"name":"Sugar Creek Township Station 42","lat":39.7762501,"lon":-85.896068},
-  {"name":"Greenwood Station 91","lat":39.6134433,"lon":-86.1040209},
 ]
 
 engines = [
